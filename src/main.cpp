54ba05606d65c21471e5b2a08b85daedf91d5f83
penPNG <graeme.roach01@yahoo.com>
2025-03-08 01:02:09 -0500

    quick refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "dmg.h"
-#include "imgui.h"
-#include "imgui_impl_sdl3.h"
-#include "imgui_impl_sdlrenderer3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_sdl3.h"
+#include "imgui/imgui_impl_sdlrenderer3.h"
 #include <stdlib.h>
 #include <vulkan/vulkan.h>
 #include <stdio.h>
