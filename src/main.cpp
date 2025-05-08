ae3fe8266af7978869f0d3cfad80693bacb53e4f
penPNG <graeme.roach01@yahoo.com>
2025-05-08 16:05:46 -0400

    now it's a context loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,119 +4,29 @@
 #include "imgui/imgui_impl_sdl3.h"
 #include "imgui/imgui_impl_sdlrenderer3.h"
 #include <stdlib.h>
+#include <stdexcept>
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <iostream>
 
 int main(char* argv) {
 	DMG* dmg = new DMG();
-
-	if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
-		printf("Error: SDL_Init(): %s\n", SDL_GetError());
-		return -1;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		throw std::runtime_error(SDL_GetError());
 	}
-
-	Uint32 window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN;
-	SDL_Window* window = SDL_CreateWindow("psiDMG", 1280, 720, window_flags);
-	if (window == NULL) {
-		printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
-		return -1;
-	}
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-	SDL_SetRenderVSync(renderer, 1);
-	if (renderer == NULL) {
-		SDL_Log("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
-		return -1;
-	}
-	SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-	SDL_ShowWindow(window);
-
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
 
-	ImGui::StyleColorsDark();
+	Context* context = new Context(1280, 720, dmg);
 
-	ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
-	ImGui_ImplSDLRenderer3_Init(renderer);
-
-	//Context* context = new Context(1280, 720, dmg);
-
-
-	// State
 	bool show_demo_window = true;
 	bool show_another_window = false;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	bool done = false;
 	while (!done) {
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			ImGui_ImplSDL3_ProcessEvent(&event);
-			if (event.type == SDL_EVENT_QUIT)
-				done = true;
-			if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
-				done = true;
-		}
-
-		if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
-			SDL_Delay(10);
-			continue;
-		}
-
-		ImGui_ImplSDLRenderer3_NewFrame();
-		ImGui_ImplSDL3_NewFrame();
-		ImGui::NewFrame();
-		Context::showDMGDebugger(dmg);
-		ImGui::ShowDemoWindow(&show_demo_window);
-
-		/*if(show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window); {
-			static float f = 0.0f;
-			static int counter = 0;
-
-			ImGui::Begin("Hello, world!");
-			ImGui::Text("This is some useful text.");
-			ImGui::Checkbox("Demo Window", &show_demo_window);
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-			ImGui::ColorEdit3("clear color", (float*)&clear_color);
-
-			if (ImGui::Button("Button"))
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.f / io.Framerate, io.Framerate);
-			ImGui::End();
-		}*/
-
-		/*if (show_another_window) {
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}*/
-
-		ImGui::Render();
-		SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		SDL_RenderClear(renderer);
-		ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
-		SDL_RenderPresent(renderer);
-	}
-
-	ImGui_ImplSDLRenderer3_Shutdown();
-	ImGui_ImplSDL3_Shutdown();
-	ImGui::DestroyContext();
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+		 done = context->update();
 
 	delete(dmg);
 
-	return 0;
+	return context->shutdown();
 }
